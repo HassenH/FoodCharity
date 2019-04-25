8dd0058bc4ea6e3cba2848de268118383f48bf8d
Hassen Hadhri <hadhri-h@live.fr>
2019-04-25 20:31:52 +0200

    FoodCharity v6

--- index.php
+++ index.php
@@ -20,19 +20,19 @@ include 'navbar.php';
       <img src="assets/img/Gateau.png" class="card-img" alt="">
       <h5>Comment ca marche ?</h5>
       <p>Agissez contre le gaspillage alimentaire en donnant vos invendus ou des denrées alimentaires dont vous ne savez que faire a une association. Choissisez l'association à laquelle vous voulez donner et convenez d'un rendez-vous pour remettre vos produits.</p>
-      <a href="">En savoir plus</a>
+      <a href="" class="btn btn-default" role="button">En savoir plus</a>
     </div>
     <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center">
       <img src="assets/img/PASTEQUE4.png" class="card-img" alt="">
       <h5>Pourquoi, pour qui ?</h5>
       <p>Foodcharity a pour mission de réduire le gaspillage alimentaire, en aidant les grandes surfaces et les particuliers à faire des dons alimentaire directement aux associations. Plus encore, le rôle de Foodcharity est de sensibiliser et d'éduquer les consommateurs pour lutter plus efficacement et durablement contre le gaspillage alimentaire, tous les jours !</p></p>
-      <a href="">En savoir plus</a>
+      <a href="" class="btn btn-default" role="button">En savoir plus</a>
     </div>
     <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center">
       <img src="assets/img/glace.png" class="card-img" alt="">
       <h5>Nos valeurs ?</h5>
       <p>Agissez contre le gaspillage alimentaire en donnant sur Optimiam</p>
-      <a href="">En savoir plus</a>
+      <a href="" class="btn btn-default" role="button">En savoir plus</a>
     </div>
   </div>
 </div>
@@ -44,10 +44,7 @@ include 'navbar.php';
         <h4 class="text-center">Quelques-unes des enseignes les plus populaires sur FoodCharity</h4>
       </div>
       <div class="container carouselPartners">
-        <div id="carousel-examp
-        Sélection de données - Élargir les possibilités de la clause WHERE
-
-        le" class="carousel slide" data-ride="carousel">
+        <div id="carousel-example" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner row w-100 mx-auto" role="listbox">
           <div class="carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active">
             <img src="assets/img/carrefour_city_logo.png" class="img-fluid mx-auto d-block" alt="img1">
@@ -94,17 +91,14 @@ include 'navbar.php';
       <h1>Notre vision</h1>
     </div>
     <div class="col-12 col-sm-6 col-md-6 col-lg-6">
-      <p>Chez FoodCharity, on oeuvre pour un monde avec du sens, où la nourriture produite est consommée et ne finit pas tragiquement gaspillée. Notre ambition c'est d'y arriver ensemble, parce qu'on croit fort au pouvoir qui réside en chacun de nous.
-        Toutes nos petites actions, mises bout à bout, peuvent avoir un impact immense ! Alors rejoins la grande famille des TooGoodeurs et deviens le héros 2 en 1 des temps modernes.</p>
-        <a href="">En savoir plus</a>
+      <p>Chez FoodCharity, on oeuvre pour un monde avec du sens, où la nourriture produite est consommée et ne finit pas tragiquement gaspillée. Notre ambition c'est d'y arriver ensemble, parce qu'on croit fort au pouvoir qui réside en chacun de nous.</p>
+      <p>Toutes nos petites actions, mises bout à bout, peuvent avoir un impact immense ! Alors rejoins la grande famille des TooGoodeurs et deviens le héros 2 en 1 des temps modernes.</p>
+        <a href="" class="btn btn-default" role="button">En savoir plus</a>
       </div>
       <div class="col-12 col-sm-6 col-md-6 col-lg-6 embed-responsive embed-responsive-16by9">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/KugQ8RqIdXE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<<<<<<< HEAD
         </div>
-=======
       </div>
->>>>>>> 8b7004283d1bb9be8b2b8ac99dc3afd52c746abf
       <div class="col-12 col-sm-12 col-md-12 col-lg-12">
         <h1>Grâce à vous, nous avons aidé à sauver : </h1>
       </div>
